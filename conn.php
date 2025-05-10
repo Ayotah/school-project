2444700fd512b689e699953c5ece64bb3be250a9
Ayotah <ayotahendali16@gmail.com>
2025-05-10 09:16:24 -0700

    mades changes

---
+++ conn.php
@@ -0,0 +1,17 @@
+<!-- <?php
+$db_server ='localhost';
+$db_username ='roots';
+$db_pass='';
+$db_name ='logindb';
+$conn='';
+try{
+    $conn=mysqli_connect($db_server,
+    $db_username,
+    $db_pass,
+    $db_name,
+);
+}
+catch(mysqli_sql_exception){
+    echo 'Could not connect <br>';
+}
+?> -->
---
+++ table.php
@@ -0,0 +1,70 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Student CRUD</title>
+</head>
+<body>
+  
+    <div class="container mt-4">
+
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Student Details
+                            <a href="student-create.php" class="btn btn-primary float-end">Add Students</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Student Name</th>
+                                    <th>Email</th>
+                                    <th>Phone</th>
+                                    <th>Course</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                              
+                                            <tr>
+                                                 <td></td>
+                                                  <td></td>
+                                                   <td></td>
+                                                    <td></td>
+                                                    <td></td>
+                                                <td>
+                                                    <a href="" class="btn btn-info btn-sm">View</a>
+                                                    <a href="" class="btn btn-success btn-sm">Edit</a>
+                                                    <form action="code.php" method="POST" class="d-inline">
+                                                        <button type="submit" name="delete_student" value="" class="btn btn-danger btn-sm">Delete</button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                    
+                                
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
